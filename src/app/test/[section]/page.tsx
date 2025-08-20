af6401572bb8c9a756db34e4ff8571b5424b6300
Your <usmansinghi786@gmail.com>
2025-08-20 20:29:05 +0500

    my

--- src/app/test/[section]/page.tsx
+++ src/app/test/[section]/page.tsx
@@ -2,5 +2,5 @@ import { promises } from "dns";
 
 export default async function Section({params}: {params: Promise<{section: string}>}) {
  const {section} = await params
-    return <h1>Test {section}</h1>;
+    return <h1> My Test {section}</h1>;
 }
